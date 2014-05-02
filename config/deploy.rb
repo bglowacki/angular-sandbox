80cc7b94bfbd026c13c9133fc639559c60a27567
Bartek Głowacki <bartosz.glowacki@kmpgroup.pl>
2014-05-02 17:17:46 +0200

    changed jquery version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,24 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'sandbox'
+set :repo_url, 'git@github.com:Vangerdahast/angular-sandbox.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/deploy/home/sandbox'
+set :deploy_to, '/home/deploy/sandbox'
+set :ssh_options, { :forward_agent => true }
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :debug
 
 # Default value for :pty is false
 # set :pty, true
@@ -32,7 +33,7 @@ set :linked_dirs, %w{node_modules app/bower_components}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
